bafbbee85e0ec64f2257edc6d2877d10c01200db
ethanc8 <echaroenpitaks@gmail.com>
2024-02-06 00:10:45 -0600

    Support many more CI configurations

---
+++ ci-tests/generate-files.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+clean() {
+    rm -r .github/workflows/*.yml
+    rm -r ci-tests/*/
+}
+
+generate() {
+    name=$1
+    dockerTag=$2
+    version=$3
+
+    cat <<EOF > ".github/workflows/$name-$version.yml" 
+on:
+  [push]
+name: $name - GNUstep $version
+jobs:
+  testbuild:
+    name: Building GNUstep
+    runs-on: ubuntu-latest
+    steps:
+      - name: Check out the build script repository
+        uses: actions/checkout@master
+      - name: Execute test script
+        run: |
+          docker build -t gnustep-build-ci-$name-$version ci-tests/$name-$version/.
+          docker run gnustep-build-ci-$name-$version
+EOF
+    mkdir -p "ci-tests/$name-$version"
+    cat <<EOF > "ci-tests/$name-$version/Dockerfile" 
+FROM $dockerTag
+
+RUN uname -a
+RUN apt-get update && apt-get install -y clang build-essential wget git sudo
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y keyboard-configuration
+RUN git clone https://github.com/ethanc8/gnustep-build
+RUN /bin/bash -c "GNUSTEP_BUILD_VERSION=$version gnustep-build/generic/build.sh"
+
+CMD [ "/bin/bash", "-c", "export PS1=allow_bash_to_run; source ~/.bashrc; ./gnustep-build/demo.sh" ]
+EOF
+}
+
+# Creates workflows and dockerfiles given the name ($1) and docker tag ($2).
+generateForOS() {
+    name=$1
+    dockerTag=$2
+
+    versions=('2021_DEC' '2023_JAN' 'STABLE' 'TRUNK')
+
+    for version in "${versions[@]}"; do
+        generate "$name" "$dockerTag" "$version"
+    done
+}
+
+clean
+
+generateForOS debsid 'debian:sid'
+generateForOS debtesting 'debian:testing'
+generateForOS deb12 'debian:12'
+generateForOS deb11 'debian:11'
+generateForOS deb10 'debian:10'
+
+generateForOS ubudevel 'ubuntu:devel'
+generateForOS ubu2310 'ubuntu:mantic'
+generateForOS ubu2204 'ubuntu:focal'
+generateForOS ubu2004 'ubuntu:jammy'
+generateForOS ubu1804 'ubuntu:bionic'
